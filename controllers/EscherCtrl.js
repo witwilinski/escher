25ef1c1222dbe876ccd0637f56b03b96fddee847
alex <alex@devmachine>
2017-08-31 02:23:41 +0200

    add node stats calculation

--- controllers/EscherCtrl.js
+++ controllers/EscherCtrl.js
@@ -3,7 +3,14 @@ app.controller("EscherCtrl", function($scope) {
 	$scope.defaultColorSchema = true;
 	
 	$scope.calculateStats = function(data) {
-	
+		var stats={}
+		for (var n in data[1].nodes) {
+			if (!stats[data[1].nodes[n].node_type])
+				stats[data[1].nodes[n].node_type]=1
+			else
+				stats[data[1].nodes[n].node_type] = stats[data[1].nodes[n].node_type]+1
+		}
+		console.log(stats)
 	}
    
    $scope.changeColorSchema = function() {
@@ -12,6 +19,7 @@ app.controller("EscherCtrl", function($scope) {
    }
    
    d3.json('../data/e_coli_core.Core%20metabolism.json', function(e, data) { 
+   $scope.calculateStats(data);
    $scope.escher_options = {
             // Just show the zoom buttons
             menu: 'zoom',
@@ -24,6 +32,15 @@ app.controller("EscherCtrl", function($scope) {
             // No tooltips
             enable_tooltips: false,
         };
+      var css = "svg.escher-svg .segment { \
+  stroke: #334E75; \
+  stroke-width: 10px; \
+  fill: none; \
+} \
+svg.escher-svg .arrowhead { \
+  fill: #334E75; \
+} \
+"
     escher.Builder(data, null, null, d3.select('#container'), $scope.escher_options);
 	});
 });
