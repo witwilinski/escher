2bac8b4bb4384b7a464aaa284f893ea6631621e3
alex <alex@devmachine>
2017-09-01 13:27:58 +0200

    - add node statistics info - loading local json files

--- controllers/EscherCtrl.js
+++ controllers/EscherCtrl.js
@@ -1,7 +1,8 @@
-app.controller("EscherCtrl", function($scope) {
+
+app.controller("EscherCtrl", [ '$scope', function($scope) {
 
 	$scope.defaultColorSchema = true;
-	
+	$scope.nodeStats = {} 
 	$scope.calculateStats = function(data) {
 		var stats={}
 		for (var n in data[1].nodes) {
@@ -10,7 +11,11 @@ app.controller("EscherCtrl", function($scope) {
 			else
 				stats[data[1].nodes[n].node_type] = stats[data[1].nodes[n].node_type]+1
 		}
-		console.log(stats)
+		
+		$scope.$apply(function() {
+			$scope.nodeStats = stats;
+		});
+		
 	}
    
    $scope.changeColorSchema = function() {
@@ -24,20 +29,38 @@ app.controller("EscherCtrl", function($scope) {
 
    }
    
-   d3.json('../data/e_coli_core.Core%20metabolism.json', function(e, data) { 
-   $scope.calculateStats(data);
-   $scope.escher_options = {
-            // Just show the zoom buttons
-            menu: 'zoom',
-            // use the smooth pan and zoom option
-            use_3d_transform: true,
-            // No editing in this map
-            enable_editing: false,
-            // No keyboard shortcuts
-            enable_keys: false,
-            // No tooltips
-            enable_tooltips: false,
-        };
-    escher.Builder(data, null, null, d3.select('#container'), $scope.escher_options);
-	});
-});
+   $scope.init = function(fileName) {
+	   d3.json(fileName, function(e, data) { 
+	   $scope.calculateStats(data);
+	   $scope.escher_options = {
+	            // Just show the zoom buttons
+	            menu: 'zoom',
+	            // use the smooth pan and zoom option
+	            use_3d_transform: true,
+	            // No editing in this map
+	            enable_editing: false,
+	            // No keyboard shortcuts
+	            enable_keys: false,
+	            // No tooltips
+	            enable_tooltips: false,
+	        };
+	    escher.Builder(data, null, null, d3.select('#container'), $scope.escher_options);
+		});
+	}
+	
+   $scope.fileChanged = function(element) {
+
+	     $scope.$apply(function(scope) {
+	         var file = element.files[0];
+	         var reader = new FileReader();
+	         reader.onload = function(e) {
+	        	 $scope.init(e.target.result);
+	            // handle onload
+	         };
+	         reader.readAsDataURL(file);
+	     });
+	};
+
+   
+	//$scope.init('../data/e_coli_core.Core%20metabolism.json');
+}]);
